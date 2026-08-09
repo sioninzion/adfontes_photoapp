2f7dbf27cea1d06057a463932f30904d9cb1d05f
sioninzion <youcandoitsion@gmail.com>
2026-08-10 02:57:56 +0900

    commenrt

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // CACHE_NAME 버전을 올려서 오래된 캐시를 확실히 폐기한다.
 // ---------------------------------------------------------------------------
 
-const CACHE_NAME = "life4cut-shell-v11";
+const CACHE_NAME = "life4cut-shell-v14";
 
 const PRECACHE_URLS = [
   "./",
@@ -38,6 +38,7 @@ const PRECACHE_URLS = [
   "./assets/decor/particle-1.png",
   "./assets/decor/particle-2.png",
   "./assets/decor/godskid.png",
+  "./assets/decor/camera.png",
   "./assets/icons/icon-180.png",
   "./assets/icons/icon-192.png",
   "./assets/icons/icon-512.png",

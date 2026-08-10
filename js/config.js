694e45d84b1e3b72de521938c9d7749e0f2faebc
sioninzion <youcandoitsion@gmail.com>
2026-08-10 17:29:56 +0900

    comment

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ export const CONFIG = {
   // 촬영
   photoCount: 6,
   selectedPhotoCount: 4,
-  countdownSeconds: 6,
+  countdownSeconds: 3,
   captureFreezeMs: 550, // 촬영 직후 결과를 잠깐 보여주는 시간
 
   // 촬영 비율: 아이패드를 가로로 들든 세로로 들든 항상 이 "세로가 긴" 비율로
